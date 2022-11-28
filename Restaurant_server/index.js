5b6e6203b9047fe90784a437776b258a834e657c
Chigozie Muonagolu <77743335+Kinggoz18@users.noreply.github.com>
2022-11-28 18:59:41 -0500

    MongoDB

--- Restaurant_server/index.js
+++ Restaurant_server/index.js
@@ -1,17 +1,26 @@
 'use strict';
 import express from 'express';
+import mongoose from 'mongoose';
 //import route from './src/routes/route';
-import { ConnectDB } from './Libraries/Database.js'
+import { ConnectMongoDB } from './Libraries/Database.js'
 
 const app = express();
 const PORT_NUM = 4000;
 
-//MSSQL Connection
-ConnectDB()
+//Mongoose Connection setup
+const url = "mongodb+srv://BillysRestaurant:Restaurant2022@billyscluster.dktaru5.mongodb.net/test"
+mongoose.Promise = global.Promise;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{console.log('Connected to MongoDB');})
+.catch((err)=>{if(err){console.log(err);}})
 
 //bodyparser setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 //Decalre the route
 
 //Have the app start listening

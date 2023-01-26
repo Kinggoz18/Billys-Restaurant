04c431e89ad889ec4da3fdce63e2abb98fc94c23
Chigozie Muonagolu <77743335+Kinggoz18@users.noreply.github.com>
2023-01-26 18:21:20 -0500

    Update AccountObject.js

--- restaurant_frontend/src/components/Objects/AccountObject.js
+++ restaurant_frontend/src/components/Objects/AccountObject.js
@@ -2,32 +2,32 @@
 import $ from 'jquery'
 
 class Account{
-    //Stores the login account Url
-    LoginUrls = {
+    //Stores the login account Url : Private
+    #LoginUrls = {
         Admin: "https://drumrockjerkapi.azure-api.net/Admin/LoginAdmin",
         Customer: "https://drumrockjerkapi.azure-api.net/Customer/LoginCustomer",
         Employee: "https://drumrockjerkapi.azure-api.net/Employee/LoginEmployee",
     };
-    //Stores the create account Url
-    CreateUrls ={
+    //Stores the create account Url : Private
+    #CreateUrls ={
         Admin: "https://drumrockjerkapi.azure-api.net/Admin/CreateAdmin",
         Customer:"https://drumrockjerkapi.azure-api.net/Customer/CreateCustomer",
         Employee: "https://drumrockjerkapi.azure-api.net/Employee/CreateEmployee",
     };
-    //Stores the delete account Url
-    DeleteUrls ={
+    //Stores the delete account Url : Private
+    #DeleteUrls ={
         Admin: "https://drumrockjerkapi.azure-api.net/Admin/DeleteAdmin",
         Customer:"https://drumrockjerkapi.azure-api.net/Customer/DeleteCustomer",
         Employee: "https://drumrockjerkapi.azure-api.net/Employee/DeleteEmployee",
     };
-    //Stores the update account Url
-    UpdateUrls ={
+    //Stores the update account Url : Private
+    #UpdateUrls ={
         Admin: "https://drumrockjerkapi.azure-api.net/Admin/UpdateAdmin",
         Customer:"https://drumrockjerkapi.azure-api.net/Customer/UpdateCustomer",
         Employee: "https://drumrockjerkapi.azure-api.net/Employee/UpdateEmployee",
     };
-    //Gets the correct GetUser Url
-    GetUserUrl(AccountType, id) {
+    //Gets the correct GetUser Url : Private
+    #GetUserUrl(AccountType, id) {
         switch(AccountType){
             case "Admin": {
                 return `https://drumrockjerkapi.azure-api.net/Admin/GetAdmin?id=${id}`;
@@ -43,33 +43,11 @@ class Account{
             }
         }
     }
-    //Gets the Account information
-    GetAccountInfro(AccountType, id){
 
-    }
-    //Creates An account
-    CreateAccount(AccountType, LoginCredentials){
-        $.ajax({
-            url: this.CreateUrls[AccountType],
-            type: "POST",
-            data: LoginCredentials,
-            success: function(data){
-                        if(data.EmailAddress === "string" && data.Password === "string"){
-                            return false;
-                        }
-                        else{
-                            return data;
-                        }
-            },
-            error: function(error){
-            console.error(error)
-            }
-        });
-    }
-    //Logs in an account 
+    // Function to Login an account : public
     LoginAccount(AccountType, LoginCredentials){
         $.ajax({
-            url: this.LoginUrls[AccountType],
+            url: this.#LoginUrls[AccountType],
             type: "POST",
             data: LoginCredentials,
             success: function(data){
@@ -85,11 +63,97 @@ class Account{
             }
         });
     }
-    //Deletes the account
-    DeleteAccount(AccountType, LoginCredentials){
-
-    }//Updates the account details
-    UpdateAccount(AccountType, LoginCredentials){
-        
+     //Gets the Account information : public
+    GetAccountInfro(AccountType, AccId){
+        $.ajax({
+            url: this.#GetUserUrl(AccountType, AccId),
+            type: "GET",
+            success: function(data){
+                        if(data.EmailAddress === "string" && data.Password === "string"){
+                            return false;
+                        }
+                        else{
+                            return data;
+                        }
+            },
+            error: function(error){
+            console.error(error)
+            }
+        });
     }
+    //Function to create an account : public
+    CreateAccount(AccountType, AccId){
+        let body = {id: AccId}
+        $.ajax({
+            url: this.#CreateUrls[AccountType],
+            type: "POST",
+            data: body,
+            success: function(data){
+                        if(data.EmailAddress === "string" && data.Password === "string"){
+                            return false;
+                        }
+                        else{
+                            return data;
+                        }
+            },
+            error: function(error){
+            console.error(error)
+            }
+        });
+    }
+    // Function to delete an account : public
+    DeleteAccount(AccountType, AccId){
+        let body = {id: AccId}
+        $.ajax({
+            url: this.#DeleteUrls[AccountType],
+            type: "DELETE",
+            data: body,
+            success: function(data){
+                        if(data.EmailAddress === "string" && data.Password === "string"){
+                            return false;
+                        }
+                        else{
+                            return data;
+                        }
+            },
+            error: function(error){
+            console.error(error)
+            }
+        });
+    }
+    //Function to Update an account details : public
+    UpdateAccount(AccountType, AccId){
+        let body = {id: AccId}
+        $.ajax({
+            url: this.#UpdateUrls[AccountType],
+            type: "PUT",
+            data: body,
+            success: function(data){
+                        if(data.EmailAddress === "string" && data.Password === "string"){
+                            return false;
+                        }
+                        else{
+                            return data;
+                        }
+            },
+            error: function(error){
+            console.error(error)
+            }
+        });
+    }
+}
+
+export class AdminAccount extends Account{
+    AccountInfo = null;
+    constructor(){
+        this.AccountInfo = {
+
+        }
+    }
+}
+export class EmployeeAccount extends Account{
+    
+}
+export class CustomerAccount extends Account{
+
 }

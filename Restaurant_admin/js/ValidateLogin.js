6bcd5d99643fe2c820a3dfe05f03dfd8c8f90704
Chigozie Muonagolu <77743335+Kinggoz18@users.noreply.github.com>
2023-04-14 17:52:00 -0400

    Update ValidateLogin.js

--- Restaurant_admin/js/ValidateLogin.js
+++ Restaurant_admin/js/ValidateLogin.js
@@ -3,8 +3,13 @@ if(!IsInStorage('AccountData')){
     window.location.replace("../public/login.html");
 }
 else{
-    let AccountData = JSON.parse(GetFromStorage('AccountData'));
-    if(AccountData['_id'] === null){
+    try{
+        let AccountData = JSON.parse(GetFromStorage('AccountData'));
+        if(AccountData['_id'] === null){
+            window.location.replace("../public/login.html");
+        }
+    }
+    catch(error){
         window.location.replace("../public/login.html");
     }
 }
